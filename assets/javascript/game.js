2ce5981e92dab0aa85770c922ba8368d35bd5c02
Emily Keen <emily.keen711@gmail.com>
2017-02-16 17:28:56 -0500

    fixed number issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,54 @@
-function idRunner(element){
-	var result = document.getElementById(element);
-	return result;
+function idRunner(element) {
+    var result = document.getElementById(element);
+    return result;
 }
 
-var win =1,
-	loss =1,
-	guessRemaing = 8,
-	computer = ["a","b", "c" , "d", "e", "f", "g", "h", "i","j", "k","l", "m", "n", "o","p",
-				 "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-	guesses= [];
-
-document.onkeyup =function(event){
-
-var computerGuess = Math.floor(Math.random() * computer.length),
-	randomWord = computer [computerGuess];
-
- var typing =document.createTextNode(event.key + ',');
-
- 	idRunner("guesses").appendChild(typing);
-
-
-if (randomWord === event.key){
-
-	idRunner("win").textContent = "Wins:" + win++;
-
-
-	if (win >= 1) {guessRemaing = 8;
-
-
-		idRunner("guessRemaing").textContent = "Guess Remaining: " + 9;
-		guessRemaing = 8;
-	}
+function resetGuesses() {
+    guessRemaing = 9;
 }
 
-else {
-		idRunner("guessRemaing").textContent =  "Guess Remaining:" + guessRemaing--;
+var win = 1;
+var loss = 1,
+    guessRemaing = 9,
+    computer = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+        "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+    ],
+    guesses = [];
 
-		if(guessRemaing <= 0) {
+document.onkeyup = function(event) {
 
-			idRunner("loss").textContent = "losses:" + loss++;
+    var computerGuess = Math.floor(Math.random() * computer.length),
+        randomWord = computer[computerGuess];
 
-			idRunner("guessRemaing").textContent = "Guesses left: " + 9;
-				guessRemaing = 8;
-		}
+    var typing = document.createTextNode(event.key + ',');
 
-}
+    idRunner("guesses").appendChild(typing);
 
 
+    if (randomWord === event.key) {
+
+        idRunner("win").textContent = "Wins:" + win++;
+        idRunner("guesses").textContent = "Your Guesses So Far: " + [];
+
+
+        if (win >= 1) {
+            idRunner("guessRemaing").textContent = "Guess Remaining: " + 9;
+            resetGuesses();
+        }
+    } else {
+        guessRemaing--;
+        idRunner("grspan").textContent = guessRemaing;
+
+
+        if (guessRemaing == 0) {
+
+            idRunner("loss").textContent = "losses:" + loss++;
+
+            idRunner("guessRemaing").textContent = "Guesses left: " + 9;
+            resetGuesses();
+
+            idRunner("guesses").textContent = "Your Guesses So Far: " + [];
+
+        }
+    }
 }
