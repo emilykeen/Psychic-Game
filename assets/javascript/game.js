297dbe44406bc11dc494779a1409d8a1ba75186a
Emily Keen <emily.keen711@gmail.com>
2017-02-17 08:59:27 -0500

    fixed wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,49 @@
-function idRunner(element){
-	var result = document.getElementById(element);
-	return result;
+function idRunner(element) {
+    var result = document.getElementById(element);
+    return result;
 }
 
 
-var win =1,
-	loss =1,
-	guessRemaining = 9,
-	computer = ["a","b", "c" , "d", "e", "f", "g", "h", "i","j", "k","l", "m", "n", "o","p",
-				 "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-	guesses= [];
+var win = 1,
+    loss = 1,
+    guessRemaining = 9,
+    computer = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+        "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+    ],
+    guesses = [];
 
-document.onkeyup =function(event){
+document.onkeyup = function(event) {
 
-var computerGuess = Math.floor(Math.random() * computer.length),
-	randomWord = computer [computerGuess];
+    var computerGuess = Math.floor(Math.random() * computer.length),
+        randomWord = computer[computerGuess];
 
- var typing =document.createTextNode(event.key + ',');
+    var typing = document.createTextNode(event.key + ',');
 
- 	idRunner("guesses").appendChild(typing);
-
-
-if (randomWord === event.key){
-
-	idRunner("win").textContent = "Wins:" + win++;
-	idRunner ("guesses").textContent= "Your Guesses So Far: " + [];
-
-	idRunner("guessRemaining").textContent = "Guesses Left: " + 9;
-				guessRemaining = 9;
-	}
- else {
-		guessRemaining--;
-		idRunner("guessRemaining").textContent =  "Guesses Left:" + guessRemaining;
+    idRunner("guesses").appendChild(typing);
 
 
-		if(guessRemaining == 0) {
+    if (randomWord === event.key) {
 
-			idRunner("loss").textContent = "Losses:" + loss++;
+        idRunner("win").textContent = "Wins:" + win++;
+        idRunner("guesses").textContent = "Your Guesses So Far: " + [];
+		idRunner("guessRemaining").textContent = "Guesses Left: " + 9;
+        	guessRemaining = 9;
+    
+} else {
+    guessRemaining--;
+    idRunner("guessRemaining").textContent = "Guesses Left:" + guessRemaining;
 
-			idRunner("guessRemaining").textContent = "Guesses Left: " + 9;
-				guessRemaining = 9;
 
-			idRunner ("guesses").textContent= "Your Guesses So Far: " + [];
+    if (guessRemaining == 0) {
 
-		}
+        idRunner("loss").textContent = "Losses:" + loss++;
+
+        idRunner("guessRemaining").textContent = "Guesses Left: " + 9;
+        guessRemaining = 9;
+
+        idRunner("guesses").textContent = "Your Guesses So Far: " + [];
+
+    }
 
 }
 
